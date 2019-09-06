74709472d7a076ca82cfc1007bb76aac91db887a
Anusca-21 <53555072+Anusca-21@users.noreply.github.com>
2019-09-06 18:07:10 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,9 +5,20 @@ public class Main{
     {
           Person A=new Person(24,"Andrei");//instanta
           Person B=new Person();
-          System.out.println(" This person "+ A.fname + " has " +A.age +" years old");
+          System.out.println(" This person "+ A.getFname()  + " has " +A.getAge() +" years old");
 
-          System.out.println(A);
-          System.out.println(B);
+         // System.out.println(A);
+         // System.out.println(B);
+        A.setFname("Adi");
+        A.setAge(30);
+        A.setLname("Nastase");
+        System.out.println(A);
+
+        Student S=new Student();
+        System.out.println(S);
+
+        Student S1=new Student(24,"Ionut","Petrescu",8.45);
+        System.out.println(S1);
+        System.out.println(S1.getMedie());
     }
 }
--- Person.java
+++ Person.java
@@ -20,6 +20,29 @@ public class Person {
         this.fname=fname;
         this.lname=lname;
     }
+    //creare getter
+    public int getAge(){
+        return  this.age;
+    }
+    public String getFname(){
+        return this.fname;
+    }
+    public String getLname(){
+        return this.lname;
+    }
+    //creare setter
+    public void setAge(int age){
+        this.age=age;
+    }
+    public void setFname(String fname){
+        this.fname=fname;
+    }
+    public void setLname(String lname){
+        this.lname=lname;
+    }
+
+
+
     public String toString(){//funtie pentru a printa datele altfel apare locatia
         return "This person name is: "+this.fname+ " "+ this.lname+"and he/she is "+
                 this.age + " years old";
